5754350130e110187f4f3ce7825ac909dc83df1a
Michael Price <dev.grumptech@gmail.com>
2023-01-30 23:17:50 -0500

    Adding enumeration for days of the week

--- src/tests/triggerTypes.test.js
+++ src/tests/triggerTypes.test.js
@@ -3,12 +3,13 @@
 /* eslint-disable brace-style */
 /* eslint-disable semi */
 /* eslint-disable new-cap */
-import {TRIGGER_STATES} from '../triggerTypes.mjs';
+import {TRIGGER_STATES, TRIGGER_DAYS} from '../triggerTypes.mjs';
 import _is from 'is-it-check';
 
 describe('Module-level tests', ()=>{
     test('Module Enumerations export expected value', ()=>{
         expect(TRIGGER_STATES).toBeInstanceOf(Object);
+        expect(TRIGGER_DAYS).toBeInstanceOf(Object);
     });
 
     describe('Module TRIGGER_STATES expected value(s)', ()=>{
@@ -22,6 +23,27 @@ describe('Module-level tests', ()=>{
         ])('Enumeration exists.', (desc, input, result) =>{
             test(desc, ()=>{
                 expect(TRIGGER_STATES).toHaveProperty(input, result);
+            });
+        });
+    });
+
+    describe('Module TRIGGER_DAYS expected value(s)', ()=>{
+        test('TRIGGER_DAYS size test', ()=>{
+            expect(Object.values(TRIGGER_DAYS).length).toBe(9);
+        });
+        describe.each([
+            ['Sunday',    'Sunday',     1],
+            ['Monday',    'Monday',     2],
+            ['Tuesday',   'Tuesday',    4],
+            ['Wednesday', 'Wednesday',  8],
+            ['Thursday',  'Thursday',  16],
+            ['Friday',    'Friday',    32],
+            ['Saturday',  'Saturday',  64],
+            ['Weekday',   'Weekday',   62],
+            ['Weekend',   'Weekend',   65],
+        ])('Enumeration exists.', (desc, input, result) =>{
+            test(desc, ()=>{
+                expect(TRIGGER_DAYS).toHaveProperty(input, result);
             });
         });
     });
--- src/triggerTypes.mjs
+++ src/triggerTypes.mjs
@@ -21,3 +21,34 @@ export const TRIGGER_STATES = {
     Triggered : 2,
     /* eslint-enable key-spacing */
 };
+
+/**
+ * @description Enumeration of the days of the week
+ * @private
+ * @readonly
+ * @enum {number}
+ * @property {number} Sunday - Sunday
+ * @property {number} Monday - Monday
+ * @property {number} Tuesday - Tuesday
+ * @property {number} Wednesday - Wednesday
+ * @property {number} Thursday - Thursday
+ * @property {number} Friday - Friday
+ * @property {number} Saturday - Saturday
+ * @property {number} Weekday - Weekday
+ * @property {number} Weekend - Weekend
+ * 
+ */
+export const TRIGGER_DAYS = {
+    /* eslint-disable key-spacing */
+    Sunday    :  1,
+    Monday    :  2,
+    Tuesday   :  4,
+    Wednesday :  8,
+    Thursday  : 16,
+    Friday    : 32,
+    Saturday  : 64,
+    /* Helpful Masks */
+    Weekday   : 62,
+    Weekend   : 65,
+    /* eslint-enable key-spacing */
+};
